b604a05a7843e960ff2f03e60534fa745ac0e5de
Deep Bag <deepbag92@gmail.com>
2020-10-12 15:07:55 +0530

    Updates

---
+++ precache-manifest.b158b614b5097ad6dcc0968432015887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a0befaeed8557758cf21e1c715676a",
+    "url": "/Resume-Reactjs/index.html"
+  },
+  {
+    "revision": "f245a98baabcbf3a3bdc",
+    "url": "/Resume-Reactjs/static/css/main.921ae51d.chunk.css"
+  },
+  {
+    "revision": "fd9f3e6222b3398d44b0",
+    "url": "/Resume-Reactjs/static/js/2.a9ae07ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Resume-Reactjs/static/js/2.a9ae07ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f245a98baabcbf3a3bdc",
+    "url": "/Resume-Reactjs/static/js/main.0f25560c.chunk.js"
+  },
+  {
+    "revision": "1c898c69c8e5747b24ca",
+    "url": "/Resume-Reactjs/static/js/runtime-main.34e3ecfc.js"
+  }
+]);
